b4a7756cbb1529ce0c0a55764e7c91053c75adea
douarmoua <mouadouarzong@gmail.com>
2023-06-08 22:35:32 -0500

    add bootstrap and forms module, ng g c budget-categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from "@angular/forms";
+import {MaterialModule} from "./material/material.module";
+import { BudgetCategoriesComponent } from './components/budget-categories/budget-categories.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetCategoriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/budget-categories/budget-categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-categories',
+  templateUrl: './budget-categories.component.html',
+  styleUrls: ['./budget-categories.component.css']
+})
+export class BudgetCategoriesComponent {
+
+}
