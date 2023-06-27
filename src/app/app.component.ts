9091c4ecc51c25e0acc4e46a01a48c0a2810de8e
douarmoua <mouadouarzong@gmail.com>
2023-06-26 22:42:56 -0500

    login and create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'productivity-frontend';
+
+  isLoggedIn:boolean = false
+  createAccount:boolean = false
+  constructor() {
+  }
 }
